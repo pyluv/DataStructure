330e2472efae3816c1acf9f29f4251ef941455e5
pengyu <pyluv@foxmail.com>
2020-04-01 15:55:59 +0800

    Double Link list

--- src/main/java/linkList/SingleLinkListDemo.java
+++ src/main/java/linkList/SingleLinkListDemo.java
@@ -9,18 +9,35 @@ package linkList;
 public class SingleLinkListDemo {
     public static void main(String[] args) {
         SingleLinkList singleLinkList = new SingleLinkList();
+        SingleLinkList targetLinkList = new SingleLinkList();
         Item item1 = new Item(1,"sss");
         Item item2 = new Item(2,"ddd");
         Item item3 = new Item(3,"fff");
-        Item item4 = new Item(3,"fff");
-//        singleLinkList.addItem(item1);
-//        singleLinkList.addItem(item2);
-//        singleLinkList.addItem(item3);
-        singleLinkList.addItemByOrder(item1);
-        singleLinkList.addItemByOrder(item3);
-        singleLinkList.addItemByOrder(item2);
+        Item item4 = new Item(4,"update");
+        singleLinkList.addItem(item1);
+        //singleLinkList.addItem(item2);
+        singleLinkList.addItem(item3);
+        targetLinkList.addItemByOrder(item2);
+        targetLinkList.addItemByOrder(item4);
+//        singleLinkList.addItemByOrder(item1);
+//        singleLinkList.addItemByOrder(item3);
+//        singleLinkList.addItemByOrder(item2);
+//        singleLinkList.addItemByOrder(item4);
+        //singleLinkList.updateItem(item4);
+        //singleLinkList.removeItem(item2);
 
-        singleLinkList.showLinkList();
+        singleLinkList.showLinkList(singleLinkList);
+        System.out.println("===============================");
+        singleLinkList.showLinkList(targetLinkList);
+        System.out.println(singleLinkList.getWorkedItemCounts());
+        //System.out.println(singleLinkList.getLastIndexItem(3));
+//        SingleLinkList reverseSingleLinkList = singleLinkList.revertLinkList(singleLinkList);
+//        reverseSingleLinkList.showLinkList(reverseSingleLinkList);
+        //singleLinkList.printReverseLinkList(singleLinkList);
+        System.out.println("===============================");
+
+        singleLinkList.mergeTwoOrderLinkList(singleLinkList, targetLinkList);
+        singleLinkList.showLinkList(targetLinkList);
 
 
     }
